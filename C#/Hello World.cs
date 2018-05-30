ede1d76e099f00ea1d68fe58657075f63840a6c7
ChrisDerWahre <chrissico@outlook.de>
2018-05-30 19:49:57 +0200

    Add files via upload

---
+++ C#/Hello World.cs
@@ -0,0 +1,11 @@
+using System;
+namespace HelloWorld
+{
+    class Hello-World
+    {
+        public static void Main(String[] args) 
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
